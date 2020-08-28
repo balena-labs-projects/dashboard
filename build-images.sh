59f71f2f8fef1d1bcc4441154f5d2df148ee3356
Chris Crocker-White <chriscw@balena.io>
2020-08-28 17:04:53 +0100

    Update build script
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

--- build-images.sh
+++ build-images.sh
@@ -5,10 +5,12 @@ function build_and_push_image () {
   local DOCKER_REPO=$1
   local BALENA_MACHINE_NAME=$2
   local DOCKER_ARCH=$3
+  local BALENA_ARCH=$4
 
-  echo "Building for machine name $BALENA_MACHINE_NAME, platform $DOCKER_ARCH using dockerfile from $DOCKERFILE_DIR, pushing to $DOCKER_REPO/dashboard"
+  echo "Building for machine name $BALENA_MACHINE_NAME, platform $DOCKER_ARCH, pushing to $DOCKER_REPO/dashboard"
 
   sed "s/%%BALENA_MACHINE_NAME%%/$BALENA_MACHINE_NAME/g" ./Dockerfile.template > ./Dockerfile.$BALENA_MACHINE_NAME
+  sed -i.bak "s/%%BALENA_ARCH%%/$BALENA_ARCH/g" ./Dockerfile.$BALENA_MACHINE_NAME && rm ./Dockerfile.$BALENA_MACHINE_NAME.bak
   docker buildx build -t $DOCKER_REPO/dashboard:$BALENA_MACHINE_NAME --platform $DOCKER_ARCH --file Dockerfile.$BALENA_MACHINE_NAME .
 
   echo "Publishing..."
@@ -21,5 +23,5 @@ function build_and_push_image () {
 # YOu can pass in a repo (such as a test docker repo) or accept the default
 DOCKER_REPO=${1:-balenablocks}
 
-# build_and_push_image $DOCKER_REPO "raspberrypi4-64" "linux/arm64"
-build_and_push_image $DOCKER_REPO "raspberrypi3" "linux/arm/v7"
+build_and_push_image $DOCKER_REPO "raspberrypi4-64" "linux/arm64" "aarch64"
+build_and_push_image $DOCKER_REPO "raspberrypi3" "linux/arm/v7" "armv7hf"
